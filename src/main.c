57b40b86dac4d909833e01dde338bd0357c4a0f4
jamesbting <unconfigured@null.spigotmc.org>
2020-06-24 15:41:28 -0700

    Added functionality to read from flnc files

--- src/main.c
+++ src/main.c
@@ -2,15 +2,18 @@
 #include "include/lexer.h"
 #include "include/parser.h"
 #include "include/visitor.h"
+#include "include/io.h"
 
+void print_help();
 
 int main(int argc, char* argv[])
 {
+   if(argc < 2) {
+	   print_help();
+   }
     lexer_T* lexer = init_lexer(
-        "var name = \"john doe\";\n"
-        "var othername = \"sarah\";\n"
-        "print(name, othername);\n"
-    );
+		    get_file_contents(argv[1])
+		    );
 
     parser_T* parser = init_parser(lexer);
     AST_T* root = parser_parse(parser);
@@ -18,4 +21,9 @@ int main(int argc, char* argv[])
     visitor_visit(visitor, root);
 
     return 0;
+}
+
+void print_help() {
+	fprintf(stderr,"Fatal: no file was passed as input. Usage: \n  $ fluentc <filename>\n");
+	exit(1);
 }
