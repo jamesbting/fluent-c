62f9e50c6f4d110f4158fed6bad757ee35884e3e
James <tingjames2000@outlook.com>
2020-06-24 19:21:55 -0700

    Added comments

--- src/main.c
+++ src/main.c
@@ -6,24 +6,27 @@
 
 void print_help();
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-   if(argc < 2) {
-	   print_help();
-   }
-    lexer_T* lexer = init_lexer(
-		    get_file_contents(argv[1])
-		    );
 
-    parser_T* parser = init_parser(lexer);
-    AST_T* root = parser_parse(parser);
-    visitor_T* visitor = init_visitor();
+    //check if a file was given
+    if (argc < 2)
+    {
+        print_help();
+    }
+
+    lexer_T *lexer = init_lexer(get_file_contents(argv[1]));
+
+    parser_T *parser = init_parser(lexer);
+    AST_T *root = parser_parse(parser);
+    visitor_T *visitor = init_visitor();
     visitor_visit(visitor, root);
 
     return 0;
 }
 
-void print_help() {
-	fprintf(stderr,"Fatal: no file was passed as input. Usage: \n  $ fluentc <filename>\n");
-	exit(1);
+void print_help()
+{
+    fprintf(stderr, "Fatal: no file was passed as input. Usage: \n  $ fluentc <filename>\n");
+    exit(1);
 }
